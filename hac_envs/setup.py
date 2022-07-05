9b8c5348fad8698c2897a773b698ed229174d1ce
Nico Gürtler <nico.guertler@tuebingen.mpg.de>
2022-07-05 17:03:28 +0200

    Add missing seeds to parameter files.

--- hac_envs/setup.py
+++ hac_envs/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(name="hac_envs",
       packages=find_packages(),
       version="1.0",
-      install_requires=["numpy", "gym", "mujoco-py==2.0.2.13"],
+      install_requires=["numpy", "gym", "mujoco-py==2.1.2.14"],
       include_package_data=True
 )
